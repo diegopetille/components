a32014ab8eaac48e51993492853ab9e34afd70b5
diegopetille <99341627+diegopetille@users.noreply.github.com>
2023-08-16 20:46:14 -0300

    4 files modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 //importação do que vai usar lá na injeção do app-root
 import { appC } from './app.component';
-import { ItemTesteComponent } from './item-teste/item-teste.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
-  declarations: [appC, ItemTesteComponent,  ItemDetailComponent],
+  declarations: [appC,  ItemDetailComponent, ResumeComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [appC],
