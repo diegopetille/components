b095c208d48bb29897b09e7c86f204ad1ad9113d
diegopetille <99341627+diegopetille@users.noreply.github.com>
2023-08-16 15:17:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# components
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vcz5sp-uvrwhk)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'minhaTag',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class appC {
+  //dados de entrada
+  itemAtual = "Violino";
+  numAtual = 22;
+  fotoCri = "https://media.licdn.com/dms/image/D4D03AQGr21LGJwmutg/profile-displayphoto-shrink_800_800/0/1685549928077?e=2147483647&v=beta&t=FiR4PjVBmRajiYN2rHCi7SfBMkuJyjSJO3Pzfk2BAHM";
+
+}
